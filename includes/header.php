5eaec67b97e43456249c6b6614fc642f8939296f
xlegolaz <ron980122@gmail.com>
2022-03-26 12:58:58 -0500

    Don't forget use fontawesome key

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- Fontawesome -->
-  <script src="https://kit.fontawesome.com/f591e99d2f.js" crossorigin="anonymous"></script>
+  <script src="" crossorigin="anonymous"></script>
 </head>
 
 <body>
